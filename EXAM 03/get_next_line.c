6652614c629897cd168309977999863efe9909e1
Boubker Ahbibe <bahbibe@e2r13p9.1337.ma>
2023-02-16 11:50:58 +0100

    update gnl

--- EXAM 03/get_next_line.c
+++ EXAM 03/get_next_line.c
@@ -6,16 +6,13 @@
 /*   By: bahbibe <bahbibe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 04:16:32 by bahbibe           #+#    #+#             */
-/*   Updated: 2023/02/14 04:17:43 by bahbibe          ###   ########.fr       */
+/*   Updated: 2023/02/16 11:50:26 by bahbibe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
-#if BUFFER_SIZE > 1
-# undef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+
 char *get_next_line(int fd)
 {
 	char *str = malloc(10000), *buf = str;
